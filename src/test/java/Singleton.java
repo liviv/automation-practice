20009afb10b4b9429fb959a4b282bd6788e1b440
VGitina <viktorialvov@gmail.com>
2020-02-28 14:23:54 +0200

    first test

---
+++ src/test/java/Singleton.java
@@ -0,0 +1,11 @@
+//public class Singleton {
+//    Class Singleton{
+//        private static Singleton inst;
+//        public static Singleton getInstance(){
+//          //  private Singleton(){
+//            if(inst == null)
+//                inst  = new Singleton();
+//            return inst;
+//      //  }
+//    }
+//}
---
+++ src/test/java/lesson09/a_own_expected_condtition/CustomExpectedConditions.java
@@ -0,0 +1,36 @@
+package lesson09.a_own_expected_condtition;
+
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+public class CustomExpectedConditions {
+    public static ExpectedCondition<WebElement> listNthElementText(By locator, int no, String expTextPart) {
+
+        return new ExpectedCondition<WebElement>() {
+            private String nthElementText = "";
+
+
+            @NullableDecl
+            @Override
+            public WebElement apply(@NullableDecl WebDriver driver) {
+                try {
+
+                    WebElement element = driver.findElements(locator).get(no);
+                    nthElementText = element.getText();
+                    return nthElementText.contains(expTextPart) ? element : null;
+                } catch (IndexOutOfBoundsException e) {
+                    return null;
+                }
+
+            }
+
+            @Override
+            public String toString() {
+                return String.format("%dth element \nof list \nto have text: %s /n while actual text was: %s\n", no, expTextPart, nthElementText);
+            }
+        };
+    }
+}
